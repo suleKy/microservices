0b1218ba06612d2560f81f01eec69dfb162df379
suleKy <sule.syl11@gmail.com>
2025-07-10 18:01:30 +0300

    Loans microservice has been created

---
+++ loans/src/main/java/com/myapp/loans/LoansApplication.java
@@ -0,0 +1,32 @@
+package com.myapp.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.myapp.loans.controller") })
+@EnableJpaRepositories("com.myapp.loans.repository")
+@EntityScan("com.myapp.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "SBank Loans microservice REST API Documentation",
+				version = "v1"
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "SBank Loans microservice REST API Documentation"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
