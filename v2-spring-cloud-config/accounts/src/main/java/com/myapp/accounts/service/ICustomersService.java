e56a98e0a91ec3c10bee1163c94dac8980c26281
suleKy <sule.syl11@gmail.com>
2025-08-10 23:12:00 +0300

    Cross cutting concerns like tracing and logging have been implemented

--- v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/ICustomersService.java
+++ v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/ICustomersService.java
@@ -9,5 +9,5 @@ public interface ICustomersService {
      * @param mobileNumber
      * @return Customer details based on a given mobile number
      */
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
 }
--- v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/impl/CustomersServiceImpl.java
+++ v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/impl/CustomersServiceImpl.java
@@ -32,7 +32,7 @@ public class CustomersServiceImpl implements ICustomersService {
      * @return Customer details based on a given mobile number
      */
     @Override
-    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
         Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
                 () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
         );
@@ -43,10 +43,10 @@ public class CustomersServiceImpl implements ICustomersService {
         CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
-        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
         customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
 
-        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
         customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
 
         return customerDetailsDto;
