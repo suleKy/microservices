d13af1f1e06c7c5da922e39b0af2585bee68e959
suleKy <sule.syl11@gmail.com>
2025-08-10 19:37:17 +0300

    Custom routing has been implemented using spring cloud gateway

--- v2-spring-cloud-config/gatewayserver/src/main/java/com/myapp/gatewayserver/GatewayserverApplication.java
+++ v2-spring-cloud-config/gatewayserver/src/main/java/com/myapp/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,34 @@ package com.myapp.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator sBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/myapp/accounts/**")
+						.filters(f -> f.rewritePath("/myapp/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/myapp/loans/**")
+						.filters(f -> f.rewritePath("/myapp/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/myapp/cards/**")
+						.filters(f -> f.rewritePath("/myapp/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS")).build();
 	}
 
 }
