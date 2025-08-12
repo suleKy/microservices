65aa7c635ab593cd8e3a14c6fd412130b2d6296a
suleKy <sule.syl11@gmail.com>
2025-08-12 18:50:51 +0300

    Docker Compose file has been updated to adapt spring cloud gateway changes

--- v2-spring-cloud-config/gatewayserver/src/main/java/com/myapp/gatewayserver/filters/RequestTraceFilter.java
+++ v2-spring-cloud-config/gatewayserver/src/main/java/com/myapp/gatewayserver/filters/RequestTraceFilter.java
@@ -24,12 +24,12 @@ public class RequestTraceFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
         if (isCorrelationIdPresent(requestHeaders)) {
-            logger.debug("eazyBank-correlation-id found in RequestTraceFilter : {}",
+            logger.debug("sBank-correlation-id found in RequestTraceFilter : {}",
                     filterUtility.getCorrelationId(requestHeaders));
         } else {
             String correlationID = generateCorrelationId();
             exchange = filterUtility.setCorrelationId(exchange, correlationID);
-            logger.debug("eazyBank-correlation-id generated in RequestTraceFilter : {}", correlationID);
+            logger.debug("sBank-correlation-id generated in RequestTraceFilter : {}", correlationID);
         }
         return chain.filter(exchange);
     }
