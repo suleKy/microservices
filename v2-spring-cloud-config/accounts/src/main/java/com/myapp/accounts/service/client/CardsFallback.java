da90fb655e9776c0da5edd69af2721a04ba00a3e
suleKy <sule.syl11@gmail.com>
2025-08-14 14:26:19 +0300

    Circuit breaker pattern has been implemented with accounts feign client

---
+++ v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/client/CardsFallback.java
@@ -0,0 +1,13 @@
+package com.myapp.accounts.service.client;
+
+import com.myapp.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient {
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
---
+++ v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/service/client/LoansFallback.java
@@ -0,0 +1,13 @@
+package com.myapp.accounts.service.client;
+
+import com.myapp.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient {
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
