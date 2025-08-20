d07cd814fd44447ded332f7d51681216c25cc331
suleKy <sule.syl11@gmail.com>
2025-08-20 14:10:10 +0300

    Rate limiter and fallback method has been implemented in accounts microservice

--- v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/controller/AccountsController.java
+++ v2-spring-cloud-config/accounts/src/main/java/com/myapp/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.myapp.accounts.dto.CustomerDto;
 import com.myapp.accounts.dto.ErrorResponseDto;
 import com.myapp.accounts.dto.ResponseDto;
 import com.myapp.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -236,11 +237,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name="getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
